d57ba3b99a6af0019a94e05e45b7458f6c36cba2
Daniel <danielpolii19@gmail.com>
2026-08-09 03:39:23 +0700

    Import Compose setContent extension

--- android/app/src/main/java/com/sagesearch/android/MainActivity.kt
+++ android/app/src/main/java/com/sagesearch/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sagesearch.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.compose.foundation.layout.Arrangement
